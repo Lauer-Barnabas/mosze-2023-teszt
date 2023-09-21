06d534436b9b114e2e84280ab2f0255067863d91
Lauer-Barnabas <115947268+Lauer-Barnabas@users.noreply.github.com>
2023-09-21 20:26:45 +0200

    Update main.cpp
    javított verzió

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //NELEMENTS nem létezik
-    std::cout << '1-100 ertekek duplazasa' //sor végén hiányzó ; , karakter konstans csak egy karaktert tartalmaz, javasolt string használata
-    for (int i = 0;) //hiányos ciklus
+    int *b = new int[N_ELEMENTS]; 
+    std::cout << "1-100 ertekek duplazasa";
+    for (int i = 0;  i < N_ELEMENTS; i++) 
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++) //hiányos kilépési feltétel
+    for (int i = 0; i < N_ELEMENTS; i++) 
     {
-        std::cout << "Ertek:" //nem ír ki értékeket a ciklus
+        std::cout << "Ertek:" << b[i] << std::endl; 
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; //inicializálatlan int
-    for (int i = 0; i < N_ELEMENTS, i++) //N_ELEMENT után vessző van, javasolt ;
+    int atlag = 0; 
+    for (int i = 0; i < N_ELEMENTS; i++) 
     {
-        atlag += b[i] //sor végén hiányzó ;
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
